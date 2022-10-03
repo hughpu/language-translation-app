c7bd366d1a255f4b50c684bea37cb11ef27f994b
蒲若坤 <hughpu@hotmail.com>
2022-10-03 19:19:56 +0800

    fix: fix google translation api error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import React, { Component, useState } from "react";
 import "./App.css";
 
-const URL_ENDPOINT = "https://google.demo";
 
 const Translator = () => {
   const [sourceText, setSourceText] = useState("你好！");
@@ -11,12 +10,29 @@ const Translator = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const api = `${URL_ENDPOINT}?sourceText=${sourceText}`;
+
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("q", sourceText);
+    encodedParams.append("source", "zh");
+    encodedParams.append("target", "en");
+    
+    const options = {
+      method: 'POST',
+      url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Accept-Encoding': 'application/gzip',
+        'X-RapidAPI-Key': '02842eb1a6msh5ab52e77b1161d9p1d0c60jsnefebafd80afb',
+        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+      },
+      data: encodedParams
+    };
+
     axios
-      .get(api)
+      .request(options)
       .then(
         result => {
-          setTargetText(result.data.targetText);
+          setTargetText(result.data.data.translations[0].translatedText);
           setError(null);
         }
       ).catch(
@@ -33,20 +49,22 @@ const Translator = () => {
   
   return (
     <form onSubmit={handleSubmit}>
-      {error && <p>error.message</p>}
+      {error && <p color="r">{error.message}</p>}
       <div>
         <label htmlFor="sourceText">Source Chinese: </label>
         <input id="sourceText" type="text" onChange={handleChange} value={sourceText} />
       </div>
       <div>
         <label htmlFor="targetText">Target English: </label>
-        <input id="targetText" type="text" />
+        <input id="targetText" type="text" value={targetText} readOnly/>
       </div>
       <button type="submit">Submit</button>
     </form>
   );
 }
 
-const App = <Translator />;
-
-export default App;
+export default class App extends Component {
+  render() {
+    return <Translator />;
+  }
+}
